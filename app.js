ac53e3f2081702977033570200d06f698fd19641
Catalina Montes de Oca <catumdeoca@gmail.com>
2022-02-05 19:52:46 -0300

    DOM

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 class Libro{
     constructor(titulo, autor, precio){
-        this.titulo = titulo; 
+        this.titulo = titulo.toUpperCase() ; 
         this.autor = autor; 
         this.precio = Number(precio);
     }
@@ -15,7 +15,16 @@ class Libro{
     }
 }
 
+let comTotal = document.getElementById("comTotal");
+let mensaje = document.createElement("div");
+
+
+
 let compraTotal = [];
+
+let costoDescuento = [];
+
+let costos = [];
 
 
 const orgulloYPrejuicio = new Libro("Orgullo y Prejuicio", "Jane Austen", 1200);
@@ -28,16 +37,17 @@ let unidades = Number(prompt("Ingrese la cantidad de unidades que va a comprar")
 
 function aplicarDescuento(precioDelLibro, descuentoMonto) {
     let descontar = precioDelLibro - descuentoMonto;
-    console.log("El costo es de " + descontar)
+    costoDescuento.push( "El costo es de " + descontar);
 }
 
 
+
 for (let i = 0; i < unidades; i++) {
-    let compra = prompt("Escriba el título del libro que desea comprar. Las opciones son:" +"\n" + "Orgullo y Prejuicio" + "\n" + "Harry Potter" + "\n" + "Caballo de Fuego");
+    let compra = prompt("Escriba el título del libro que desea comprar. Las opciones son:" +"\n" + "Orgullo y Prejuicio" + "\n" + "Harry Potter" + "\n" + "Caballo de Fuego").toUpperCase();
     
 
     if (orgulloYPrejuicio.comprar(compra)) {
-        compraTotal.push(new Libro(orgulloYPrejuicio.título, orgulloYPrejuicio.autor, orgulloYPrejuicio.precio));
+        compraTotal.push(new Libro(orgulloYPrejuicio.titulo, orgulloYPrejuicio.autor, orgulloYPrejuicio.precio));
 
         let descuento = (confirm("¿Tiene algún descuento?"));
 
@@ -46,14 +56,12 @@ for (let i = 0; i < unidades; i++) {
 
             aplicarDescuento(orgulloYPrejuicio.precio, montoDescuento);
         } else{
-            console.log("El costo es de " + orgulloYPrejuicio.precio);
+            costos.push("El costo es de " + orgulloYPrejuicio.precio);
         }
-
-        
     }
 
     if (harryPotter.comprar(compra)) {
-        compraTotal.push(new Libro(harryPotter.título, harryPotter.autor, harryPotter.precio));
+        compraTotal.push(new Libro(harryPotter.titulo, harryPotter.autor, harryPotter.precio));
 
         let descuento = (confirm("¿Tiene algún descuento?"));
 
@@ -62,14 +70,12 @@ for (let i = 0; i < unidades; i++) {
 
             aplicarDescuento(harryPotter.precio, montoDescuento);
         } else{
-            console.log("El costo es de " + harryPotter.precio)
+            costos.push("El costo es de " + harryPotter.precio)
         }
-
-        
     }
 
     if (caballoDeFuego.comprar(compra)) {
-        compraTotal.push(new Libro(caballoDeFuego.título, caballoDeFuego.autor, caballoDeFuego.precio));
+        compraTotal.push(new Libro(caballoDeFuego.titulo, caballoDeFuego.autor, caballoDeFuego.precio));
 
         let descuento = (confirm("¿Tiene algún descuento?"));
 
@@ -78,9 +84,15 @@ for (let i = 0; i < unidades; i++) {
 
             aplicarDescuento(caballoDeFuego.precio, montoDescuento);
         } else{
-            console.log("El costo es de " + caballoDeFuego.precio);
+            costos.push("El costo es de " + caballoDeFuego.precio);
         }
     }
 }
 
-console.log(compraTotal)
+
+mensaje.innerHTML = costoDescuento + ", " + costos;
+
+comTotal.appendChild(mensaje);
+
+console.log(compraTotal);
+
