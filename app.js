bde6f15a472f37d5fd95966d7b30299aebf0e12d
Catalina Montes de Oca <catumdeoca@gmail.com>
2022-02-12 23:46:03 -0300

    eventos

--- app.js
+++ app.js
@@ -2,97 +2,115 @@
 
 class Libro{
     constructor(titulo, autor, precio){
-        this.titulo = titulo.toUpperCase() ; 
+        this.id = id;
+        this.titulo = titulo.toUpperCase(); 
         this.autor = autor; 
-        this.precio = Number(precio);
+        this.precio = precio;
     }
 
-    comprar(pedido){
-        if (this.titulo == pedido) {
-            return true
-        } 
-        return false
-    }
-}
-
-let comTotal = document.getElementById("comTotal");
-let mensaje = document.createElement("div");
-
-
-
-let compraTotal = [];
-
-let costoDescuento = [];
-
-let costos = [];
-
-
-const orgulloYPrejuicio = new Libro("Orgullo y Prejuicio", "Jane Austen", 1200);
-
-const harryPotter = new Libro("Harry Potter", "J. K. Rowling", 1450);
-
-const caballoDeFuego = new Libro("Caballo de Fuego", "Florencia Bonelli", 1300);
-
-let unidades = Number(prompt("Ingrese la cantidad de unidades que va a comprar"));
-
-function aplicarDescuento(precioDelLibro, descuentoMonto) {
-    let descontar = precioDelLibro - descuentoMonto;
-    costoDescuento.push( "El costo es de " + descontar);
+    // comprar(pedido){
+    //     if (this.titulo == pedido) {
+    //         return true
+    //     } 
+    //     return false   .toUpperCase()
+    // }
 }
 
 
+let libros = [
+    {
+        id:1,
+        titulo:"Orgullo y Prejuicio".toUpperCase(),
+        autor:"Jane Austen",
+        precio:1200,
+    },
+    {
+        id:2,
+        titulo:"Harry Potter".toUpperCase(),
+        autor:"J. K. Rowling",
+        precio:1450,
+    },
+    {
+        id:3,
+        titulo:"Caballo de Fuego".toUpperCase(),
+        autor:"Florencia Bonelli",
+        precio:1300,
+    },
+]
 
-for (let i = 0; i < unidades; i++) {
-    let compra = prompt("Escriba el título del libro que desea comprar. Las opciones son:" +"\n" + "Orgullo y Prejuicio" + "\n" + "Harry Potter" + "\n" + "Caballo de Fuego").toUpperCase();
-    
+const carrito = [];
 
-    if (orgulloYPrejuicio.comprar(compra)) {
-        compraTotal.push(new Libro(orgulloYPrejuicio.titulo, orgulloYPrejuicio.autor, orgulloYPrejuicio.precio));
+let container = document.getElementById("container");
 
-        let descuento = (confirm("¿Tiene algún descuento?"));
+let mensaje = document.getElementById("mensaje");
 
-        if (descuento == true) {
-            let montoDescuento = Number(prompt("Ingrese el monto de su descuento"));
+//Agregar al carrito
 
-            aplicarDescuento(orgulloYPrejuicio.precio, montoDescuento);
-        } else{
-            costos.push("El costo es de " + orgulloYPrejuicio.precio);
-        }
+function agregarAlCarrito() {
+    let inputProducto = document.getElementById("inputProducto");
+
+    let libro = libros.find((e) => e.titulo === inputProducto.value.toUpperCase());
+    //
+
+    if(libro){
+        carrito.push(libro);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        mostrarProductos();
+    } else {
+        let msj = document.createElement("div");
+        msj.innerHTML = `<div>El producto no fue encontrado. Por favor ingrese otro producto.</div>`;
+        container.appendChild(msj)
     }
+}
 
-    if (harryPotter.comprar(compra)) {
-        compraTotal.push(new Libro(harryPotter.titulo, harryPotter.autor, harryPotter.precio));
+//borrar productos
 
-        let descuento = (confirm("¿Tiene algún descuento?"));
+function vaciarCarrito() {
+    localStorage.clear();
 
-        if (descuento == true) {
-            let montoDescuento = Number(prompt("Ingrese el monto de su descuento"));
+    carrito = [];
 
-            aplicarDescuento(harryPotter.precio, montoDescuento);
-        } else{
-            costos.push("El costo es de " + harryPotter.precio)
-        }
-    }
+    container.innerHTML = "";
 
-    if (caballoDeFuego.comprar(compra)) {
-        compraTotal.push(new Libro(caballoDeFuego.titulo, caballoDeFuego.autor, caballoDeFuego.precio));
+    mostrarProductos();
+}
 
-        let descuento = (confirm("¿Tiene algún descuento?"));
 
-        if (descuento == true) {
-            let montoDescuento = Number(prompt("Ingrese el monto de su descuento"));
+//mostrar productos 
 
-            aplicarDescuento(caballoDeFuego.precio, montoDescuento);
-        } else{
-            costos.push("El costo es de " + caballoDeFuego.precio);
+function mostrarProductos() {
+    const carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+
+    if(carritoStorage !== null){
+        let container = document.getElementById("mensaje");
+        container.innerHTML = "";
+        for (const libro of carritoStorage) {
+            let cont = document.createElement("div");
+            cont.id = libro.id;
+            cont.innerHTML = `
+                    <div>
+                        <h3>${libro.titulo}</h3>
+                        <p>${libro.autor}</p>
+                        <h4>$${libro.precio}</h4>
+                    </div>
+                    `
+                    // let boton = document.createElement("button");
+                    // boton.id = libro.id;
+                    // boton.innerHTML = "Eliminar";
+                    // boton.addEventListener("click", vaciarCarrito);
+            
+            cont.appendChild(boton);
+            mensaje.appendChild(cont);
         }
     }
 }
 
 
-mensaje.innerHTML = costoDescuento + ", " + costos;
+//Eventos Botones
 
-comTotal.appendChild(mensaje);
+let botonAgregar = document.getElementById("botonAgregar");
+let botonVaciarCarrito = document.getElementById("botonVaciarCarrito");
 
-console.log(compraTotal);
+botonAgregar.addEventListener("click", agregarAlCarrito);
+botonVaciarCarrito.addEventListener("click", vaciarCarrito);
 
