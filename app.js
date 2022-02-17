11db5b5f3531ec87dec860101a31ee4f0a917456
Catalina Montes de Oca <catumdeoca@gmail.com>
2022-02-16 22:22:13 -0300

    segunda entrega

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Carrito libros
 
 class Libro{
-    constructor(titulo, autor, precio){
+    constructor(id, titulo, autor, precio){
         this.id = id;
         this.titulo = titulo.toUpperCase(); 
         this.autor = autor; 
@@ -19,38 +19,42 @@ class Libro{
 
 let libros = [
     {
-        id:1,
+        id:0,
         titulo:"Orgullo y Prejuicio".toUpperCase(),
         autor:"Jane Austen",
         precio:1200,
     },
     {
-        id:2,
+        id:1,
         titulo:"Harry Potter".toUpperCase(),
         autor:"J. K. Rowling",
         precio:1450,
     },
     {
-        id:3,
+        id:2,
         titulo:"Caballo de Fuego".toUpperCase(),
         autor:"Florencia Bonelli",
         precio:1300,
     },
 ]
 
-const carrito = [];
+let carrito = [];
 
 let container = document.getElementById("container");
 
 let mensaje = document.getElementById("mensaje");
 
+let precioDiv = document.getElementById("precio");
+
+
 //Agregar al carrito
 
-function agregarAlCarrito() {
+function agregarAlCarrito(evt) {
+    container.innerHTML = ``;
+
     let inputProducto = document.getElementById("inputProducto");
 
     let libro = libros.find((e) => e.titulo === inputProducto.value.toUpperCase());
-    //
 
     if(libro){
         carrito.push(libro);
@@ -59,8 +63,11 @@ function agregarAlCarrito() {
     } else {
         let msj = document.createElement("div");
         msj.innerHTML = `<div>El producto no fue encontrado. Por favor ingrese otro producto.</div>`;
-        container.appendChild(msj)
+        container.appendChild(msj);
+        evt.preventDefault();
     }
+
+    console.log(carrito);
 }
 
 //borrar productos
@@ -75,16 +82,30 @@ function vaciarCarrito() {
     mostrarProductos();
 }
 
+//Borrar un producto
+
+// function borrarProd() {
+
+//     localStorage.removeItem();
+
+//     carrito = [];
+
+//     container.innerHTML = "";
+
+//     mostrarProductos();
+// }
+
 
 //mostrar productos 
 
 function mostrarProductos() {
-    const carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+
+    let carritoStorage = JSON.parse(localStorage.getItem("carrito"));
 
     if(carritoStorage !== null){
-        let container = document.getElementById("mensaje");
+        let container = document.getElementById("container");
         container.innerHTML = "";
-        for (const libro of carritoStorage) {
+        for (let libro of carritoStorage) {
             let cont = document.createElement("div");
             cont.id = libro.id;
             cont.innerHTML = `
@@ -94,13 +115,14 @@ function mostrarProductos() {
                         <h4>$${libro.precio}</h4>
                     </div>
                     `
-                    // let boton = document.createElement("button");
-                    // boton.id = libro.id;
-                    // boton.innerHTML = "Eliminar";
-                    // boton.addEventListener("click", vaciarCarrito);
+                    let boton = document.createElement("button");
+                    boton.id = libro.id;
+                    boton.innerHTML = "Eliminar";
+                    //boton.addEventListener("click", borrarProd);
             
             cont.appendChild(boton);
-            mensaje.appendChild(cont);
+            container.appendChild(cont);
+            //e.preventDefault();
         }
     }
 }
