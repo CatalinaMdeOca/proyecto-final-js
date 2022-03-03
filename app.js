2aabc3d70fe47d8019e9677c903d24e21acb5f69
Catalina Montes de Oca <catumdeoca@gmail.com>
2022-03-02 23:18:31 -0300

    ajax

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 //Carrito libros
 
 class Libro{
-    constructor(id, titulo, autor, precio){
+    constructor(id, titulo, autor, precio, cantidad){
         this.id = id;
         this.titulo = titulo.toUpperCase(); 
         this.autor = autor; 
         this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    agregarUnidad(){
+        this.cantidad++;
+    }
+
+    quitarUnidad(){
+        this.cantidad--;
     }
 }
 
 
-let libros = [
-    {
-        id:0,
-        titulo:"Orgullo y Prejuicio".toUpperCase(),
-        autor:"Jane Austen",
-        precio:1200,
-    },
-    {
-        id:1,
-        titulo:"Harry Potter".toUpperCase(),
-        autor:"J. K. Rowling",
-        precio:1450,
-    },
-    {
-        id:2,
-        titulo:"Caballo de Fuego".toUpperCase(),
-        autor:"Florencia Bonelli",
-        precio:1300,
-    },
-]
+
+
+// let libros = [
+//     {
+//         id:0,
+//         titulo:"Orgullo y Prejuicio".toUpperCase(),
+//         autor:"Jane Austen",
+//         precio:1200,
+//     },
+//     {
+//         id:1,
+//         titulo:"Harry Potter".toUpperCase(),
+//         autor:"J. K. Rowling",
+//         precio:1450,
+//     },
+//     {
+//         id:2,
+//         titulo:"Caballo de Fuego".toUpperCase(),
+//         autor:"Florencia Bonelli",
+//         precio:1300,
+//     },
+// ]
 
 let carrito = [];
 
@@ -40,28 +51,66 @@ let mensaje = document.getElementById("mensaje");
 let precioDiv = document.getElementById("precio");
 
 
+//IMPRIMIR PROD HTML
+
+$(document).ready(function () {
+    //$("#botonAgregar").click(function (e) { 
+            //e.preventDefault();
+    
+            $.get("libros.json", function (data) {
+                    console.log(data);
+    
+                    $.each(data, function (index, libro) { 
+                        $("#contenedor").append(`<div       style="margin: 15px;" id="card">
+                                                <h3>${libro.titulo}</h3>
+                                                <h5>${libro.autor}</h5>
+                                                <h3>$${libro.precio}</h3>
+                                                `);
+
+                        // $(`agregar${libro.id}`).click(function () { 
+                            // <button type="button" id="agregar${libro.id}">Agregar</button>
+                            //                     </div>
+                        // });
+                    });
+    
+                }
+            );
+        //});
+    });
+
+
 //Agregar al carrito
 
 function agregarAlCarrito(evt) {
     evt.preventDefault();
+
+    let inputProducto = document.getElementById("selectProducto");
+
+    $.get("libros.json", function (data) {
+            $.each(data, function (index, libro) { 
+                
+                    if(inputProducto.value == libro.id){
+                        
+                        carrito.push(libro);
+                        localStorage.setItem("carrito", JSON.stringify(carrito));
+                        //mostrarProductos();
+                    } else {
+                        $("#container").append(`<div>El producto no fue encontrado. Por favor ingrese otro producto.</div>`);
+                    }
+                
+            });
+    });
+
     container.innerHTML = ``;
 
-    let inputProducto = document.getElementById("inputProducto");
-
-    let libro = libros.find((e) => e.titulo === inputProducto.value.toUpperCase());
-
-    if(libro){
-        carrito.push(libro);
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-        mostrarProductos();
-    } else {
-        let msj = document.createElement("div");
-        msj.innerHTML = `<div>El producto no fue encontrado. Por favor ingrese otro producto.</div>`;
-        container.appendChild(msj);
-    }
-
+    //let libro = libros.find((e) => e.titulo === inputProducto.value.toUpperCase());
     console.log(carrito);
+
+    //mostrarProductos();
 }
+
+
+
 
 //borrar productos
 
@@ -71,28 +120,8 @@ function vaciarCarrito() {
     carrito = [];
 
     container.innerHTML = "";
-
-    mostrarProductos();
 }
 
-
-//mostrar productos 
-
-function mostrarProductos() {
-
-    let carritoStorage = JSON.parse(localStorage.getItem("carrito"));
-
-        for (let libro of carritoStorage) {
-
-            $("#container").append(`
-            <div id="card">
-                <h3>${libro.titulo}</h3>
-                <p>${libro.autor}</p>
-                <h4>$${libro.precio}</h4>
-            </div>
-            `);
-        }
-    }
 
 
 //Eventos Botones
